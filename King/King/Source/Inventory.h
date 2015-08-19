875ad5c078ecdf744d53863e7f658ed424749180
Marcus <malitosi@hotmail.com>
2015-08-19 23:07:58 +0800

    Inventory class

---
+++ King/King/Source/Inventory.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <vector>
+#include "Item.h"
+
+using std::vector;
+
+class CInventory
+{
+private:
+	vector<CItem*> myInventory;
+
+public:
+	CInventory(void);
+	~CInventory(void);
+
+	bool addItem(CItem* itemToAdd);
+	void removeItem(int itemID);
+	void swapItem(int item1ID, int item2ID);
+
+	void setSize(int size);
+	int getSize(void);
+};
+
